fe2c089b9bdfdf783ea4e3f6723396ffcd3b9aa8
Marko Dragicevic <marko.dragicevic@levi9.com>
2024-08-31 10:04:59 +0200

    feat: home page changes

--- src/components/Home/StartBanner/StartBanner.js
+++ src/components/Home/StartBanner/StartBanner.js
@@ -7,10 +7,10 @@ export const StartBanner = () => {
       <div className="container">
         <div className="row fullscreen align-items-center justify-content-between">
           <div className="col-lg-6 col-md-6 banner-left">
-            <h6 className="text-white text-uppercase mb-3">Escape the Routine</h6>
+            <h6 className="text-white text-uppercase mb-3">Discover Your Next Adventure</h6>
             <h1 className="text-white display-4 font-weight-bold">Booking</h1>
             <p className="text-white lead">
-              "Your Journey, Our Expertise: Where Booking Meets Brilliance!"
+              "Explore the World with Confidence: Your Perfect Stay Awaits!"
             </p>
           </div>
           <div className="col-lg-4 col-md-6 banner-right mt-4">
